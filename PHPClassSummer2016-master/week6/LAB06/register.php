8b82e671782ac4a7b6b44d957ebbdb783b47a078
Wyatt <Wyatt-PC>
2016-09-05 14:37:46 -0400

    Formating

--- PHPClassSummer2016-master/week6/LAB06/register.php
+++ PHPClassSummer2016-master/week6/LAB06/register.php
@@ -9,14 +9,17 @@
         <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
 
         <!--Let browser know website is optimized for mobile-->
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/> 
 
     </head>
     <body style="width:50%; margin:0 auto;">
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script type="text/javascript" src="js/materialize.min.js"></script>
         <br>
-
+ 
+        
+        
+        
         <?php
         include './functions/cart.php';
         include './functions/dbconnect.php';
