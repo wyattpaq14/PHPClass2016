1c921e074053c26be6581befb34ab99141f53f6d
PaquinWyattA <PaquinWyattA@Wyatt-PC>
2016-09-10 18:33:04 -0400

    Lab 06 Commit

--- PHPClassSummer2016-master/week6/LAB06/templates/admin-templates/add_c.html.php
+++ PHPClassSummer2016-master/week6/LAB06/templates/admin-templates/add_c.html.php
@@ -4,17 +4,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 ?>
 <br>
 <form action="#" method="post">
     <div class="row">
         <div class="input-field col s12">
-            <input id="email" type="email" name="catname">
-            <label for="name">Category Name</label>
+            <input id="first_name" type="text" name="catname">
+            <label for="first_name">Category Name</label>
         </div>
     </div>
-
+    
     <input type="hidden" name="action" value="add_cc" />
     <button type="submit" class="waves-effect waves-light btn-large red" value="add_cc" style="width:100%; margin:0 auto;">Add Category</button>
-
+<?php $catNam = filter_input(INPUT_POST, 'catname') ?>
 </form>
