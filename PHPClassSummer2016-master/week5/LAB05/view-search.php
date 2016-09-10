125c767fb535ba5e1cb34be62b42ee9bc15a9aa2
PaquinWyattA <PaquinWyattA@Wyatt-PC>
2016-09-09 21:32:08 -0400

    Implemented curl

--- PHPClassSummer2016-master/week5/LAB05/view-search.php
+++ PHPClassSummer2016-master/week5/LAB05/view-search.php
@@ -40,8 +40,32 @@
         </div>
 
 
+        <?php
+        /*
+         * http://php.net/manual/en/curl.examples.php
+         * http://php.net/manual/en/curl.examples-basic.php
+         */
+        // create curl resource 
+        $curl = curl_init();
 
+        // set url 
+        curl_setopt($curl, CURLOPT_URL, "www.example.com");
 
+        //return the transfer as a string 
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        // $output contains the output string 
+        $output = curl_exec($curl);
+
+        // close curl resource to free up system resources 
+        curl_close($curl);
+        
+        ?>
+        <curloutput>
+        <?php echo $output; ?>
+        
+            
+        </curloutput>
 
 
 </body>
