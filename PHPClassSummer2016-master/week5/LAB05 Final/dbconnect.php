c0a9bc2c90db7540b6e03dbcb50f2a848d899e35
PaquinWyattA <PaquinWyattA@Wyatt-PC>
2016-09-18 13:46:19 -0400

    Lab 06 Commit

---
+++ PHPClassSummer2016-master/week5/LAB05 Final/dbconnect.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Function to extablish a databse connection
+ * 
+ * @return PDO Object
+ */
+function getDatabase() {
+    $config = array(
+        'DB_DNS' => 'mysql:host=localhost;port=3306;dbname=PHPClassSummer2016',
+        'DB_USER' => 'root',
+        'DB_PASSWORD' => ''
+    );
+
+    try {
+        /* Create a Database connection and 
+         * save it into the variable */
+        $db = new PDO($config['DB_DNS'], $config['DB_USER'], $config['DB_PASSWORD']);
+        $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    } catch (Exception $ex) {
+        /* If the connection fails we will close the 
+         * connection by setting the variable to null */
+        $db = null;
+        echo "DB ERROR!";
+    }
+
+    return $db;
+}
---
+++ PHPClassSummer2016-master/week5/LAB05 Final/functions.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * A method to check if a Post request has been made.
+ *    
+ * @return boolean
+ */
+include './dbconnect.php';
+
+function isPostRequest() {
+    return ( filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST' );
+}
+
+function getSiteOutput($url) {
+    $curl = curl_init();
+
+    // set url 
+    curl_setopt($curl, CURLOPT_URL, $url);
+
+    //return the transfer as a string 
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    // $output contains the output string 
+    $output = curl_exec($curl);
+
+    // close curl resource to free up system resources 
+    curl_close($curl);
+
+    return $output;
+}
+
+function collectLinks($output) {
+    $newRegex = "/(https?:\/\/[\da-z\.-]+\.[a-z\.]{2,6}[\/\w \.-]+)/";
+
+    preg_match_all($newRegex, $output, $linkMatches); //gets html
+
+    $removeDuplicates = array_unique($linkMatches[0]); //arrayunique removes duplicates
+
+    return $removeDuplicates;
+}
+
+
+function insertSite($url, $links) {
+    $db = getDatabase();
+
+
+
+    $stmt = $db->prepare("INSERT INTO sites SET site = :link, date = NOW()");
+
+        $site_id = $db->lastInsertId();
+
+
+    $binds = array(
+        ":link" => $url
+    );
+
+
+    if ($stmt->execute($binds) && $stmt->rowCount() > 0) {
+        $result = 'Site Added';
+
+
+                $site_id = $db->lastInsertId();
+        
+
+        $stmt = $db->prepare("INSERT INTO sitelinks SET link = :link, site_id = :site_id");
+
+        foreach ($links as $link) {
+            $binds = array(":link" => $link,
+                ":site_id" => $site_id
+            );
+            $stmt->execute($binds);
+        }
+
+
+
+    } else {
+        $result = 'Site Not Added';
+    }
+}
